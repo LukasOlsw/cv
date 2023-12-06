9c87051fd7408c119ad5699dfe20e9059411d7af
Lukasz Olszewicz <lukasz.olszewicz@gmail.com>
2023-12-06 21:56:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Project</p>
+        <p>Project test</p>
         <p>CV</p>
         <a
           className="App-link"
